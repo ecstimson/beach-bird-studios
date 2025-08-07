3151be37ddac943f2f1983d1cebbda3ecbd22136
Eric Stimson <ericstimson@Erics-MacBook-Pro.local>
2025-08-07 15:39:41 -0400

    Initial commit: Beach Bird Studios setup with Figma design

---
+++ src/components/Box.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Gmt4 from "./2025-13-58-35-GMT-4.svg";
+
+export const Box = (): JSX.Element => {
+  return (
+    <div className="w-[2120px] h-[3047px]">
+      <img
+        className="fixed w-[2120px] h-[3047px] top-0 left-0"
+        alt="Beach bird studios"
+        src={Gmt4}
+      />
+    </div>
+  );
+};
